37adee1039d23e13c174447bfdc9bf6a49f79c67
Andrey Mirt <nrmously@gmail.com>
2024-03-22 02:37:49 +0300

    First commit: new apps and models.

---
+++ blogicum/blog/models.py
@@ -0,0 +1,92 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class BaseBlogModel(models.Model):
+    """Абстрактная модель. Добвляет флаг is_published и created_at"""
+
+    is_published = models.BooleanField(
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
+        verbose_name='Опубликовано'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('created_at', )
+
+
+class Category(BaseBlogModel):
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Заголовок'
+    )
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(
+        unique=True,
+        help_text='Идентификатор страницы для URL; разрешены символы '
+                  'латиницы, цифры, дефис и подчёркивание.',
+        verbose_name='Идентификатор'
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Location(BaseBlogModel):
+    name = models.CharField(max_length=256, verbose_name='Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(BaseBlogModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        help_text='Если установить дату и время в будущем — можно делать '
+                  'отложенные публикации.',
+        verbose_name='Дата и время публикации'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
+    )
+    location = models.ForeignKey(
+        Location,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Местоположение'
+    )
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        default_related_name = 'posts'
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date', )
+
+    def __str__(self):
+        return self.title
---
+++ blogicum/blog/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.utils.timezone import now
+
+from .models import Post
+
+
+def filters():
+    return Post.objects.select_related(
+        'author', 'category', 'location'
+    ).filter(
+        is_published=True,
+        pub_date__lt=now(),
+        category__is_published=True,
+    )
+
+
+def index(request: HttpResponse) -> HttpResponse:
+    posts = filters()
+    template = 'blog/index.html'
+    context = {'post_list': posts[0:5]}
+    return render(request, template, context)
+
+
+def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+    post = get_object_or_404(filters(), id=id)
+    template = 'blog/detail.html'
+    context = {'post': post}
+    return render(request, template, context)
+
+
+def category_posts(request: HttpRequest, category: str) -> HttpResponse:
+    post_list = get_list_or_404(
+        Post.objects.select_related(
+            'category'
+        ).filter(
+            is_published=True,
+            category__is_published=True,
+            pub_date__lt=now(),
+            category__slug=category
+        )
+    )
+    template = 'blog/category.html'
+    context = {'post_list': post_list}
+    return render(request, template, context)
